1a3ede32fd64485c249b43b1e4df676cac7a4529
Kevin Lewis <kvn@lws.io>
2019-02-10 20:04:37 +0000

    voting works

--- pages/game/_id/index.vue
+++ pages/game/_id/index.vue
@@ -2,28 +2,32 @@
   <div>
     <div v-if="game.state">
       <h1 class="title">Round {{game.state.round}}: {{game.state.phase}}</h1>
+
       <div v-if="game.state.phase == 'play'">
         <p>Have a chat with your partner. Remember that you'll both have to secretly choose whether you will cooperate or defect.</p>
       </div>
-      <!-- <div v-if="game.state.phase == 'vote'">
-        <label for="pos">What is your table position?</label>
-        <input type="text" id="pos" v-model="move.pos" required>
-        <label for="vote">What is your move this turn?</label>
-        <select v-model="move.vote" id="vote" required>
-          <option value="1">cooperate</option>
-          <option value="-1">defect</option>
-        </select>
-        <label for="vote-why">Why did you make this choice?</label>
-        <textarea v-model="move.voteWhy" id="vote-why" required></textarea>
-        <label for="op-vote">What do you think your opponent will do this turn?</label>
-        <select v-model="move.opVote" id="op-vote" required>
-          <option value="1">cooperate</option>
-          <option value="-1">defect</option>
-        </select>
-        <label for="op-vote-why">Why do you think this?</label>
-        <textarea v-model="move.opVoteWhy" id="op-vote-why" required></textarea>
-        <input type="submit" value="Submit">
-      </div> -->
+
+      <div v-if="game.state.phase == 'vote'">
+        <form @submit.prevent="voteSubmit" v-if='!subtmittedVote'>
+          <label for="vote">What is your move this turn?</label>
+          <select v-model="move.vote" id="vote" required>
+            <option value="1">cooperate</option>
+            <option value="-1">defect</option>
+          </select>
+          <label for="vote-why">Why did you make this choice?</label>
+          <textarea v-model="move.voteWhy" id="vote-why" required></textarea>
+          <label for="op-vote">What do you think your opponent will do this turn?</label>
+          <select v-model="move.opVote" id="op-vote" required>
+            <option value="1">cooperate</option>
+            <option value="-1">defect</option>
+          </select>
+          <label for="op-vote-why">Why do you think this?</label>
+          <textarea v-model="move.opVoteWhy" id="op-vote-why" required></textarea>
+          <input type="submit" value="Submit">
+        </form>
+        <p v-else>Thanks for submitting your vote. Hang tight for the reveal.</p>
+      </div>
+
       <div v-if="game.state.phase == 'move'">
         <form @submit.prevent='roundEndSubmit' v-if='!submittedPos'>
           <label for="pos">What is your new table position?</label>
@@ -32,6 +36,7 @@
         </form>
         <p v-else>Thanks for submitting your position. Hang tight for the start of the next round.</p>
       </div>
+
       <div id="dev">
         <h2>Debugging</h2>
         <ul>
@@ -39,7 +44,7 @@
           <li>Game: {{game.name}} ({{game['.key']}})</li>
           <li>User score: {{user.score}}</li>
           <li>All scores: <span v-for='score in user.scores' :key='score'>{{score}}, </span></li>
-          <li>Current pos: {{currentPosition}}</li>
+          <li>Current pos: {{user.current}}</li>
           <li>Current side: {{user.side}}</li>
         </ul>
       </div>
@@ -55,10 +60,10 @@ export default {
     return {
       game: {},
       user: {},
-      voted: false,
       move: {},
       movePos: undefined,
-      submittedPos: false
+      submittedPos: false,
+      subtmittedVote: false,
     }
   },
   firestore() {
@@ -74,19 +79,35 @@ export default {
           ...this.user.scores,
           [this.game.state.round]: parseInt(this.movePos)
         },
-        score: this.user.score + parseInt(this.movePos) 
+        score: this.user.score + parseInt(this.movePos),
+        current: parseInt(this.movePos)
       }).then(() => {
         this.$toast.success('Submitted successfully')
-        this.submittedPos = true; 
+        this.submittedPos = true;
+        this.subtmittedVote = false; 
       });
+    },
+    voteSubmit() {
+      this.$firestore.user.update({
+        moves: {
+          ...this.user.moves,
+          [this.game.state.round]: {
+            ...this.move,
+            position: this.user.current,
+            side: this.user.side
+          }
+        }
+      }).then(() => {
+        this.$toast.success('Successfully voted')
+        this.subtmittedVote = true;
+        this.submittedPos = false;
+      })
+      
     }
   },
   computed: {
     userInState() {
       return this.$store.getters.user
-    },
-    currentPosition() {
-      return this.user.scores[Object.keys(this.user.scores).length - 1]
     }
   }
 }
