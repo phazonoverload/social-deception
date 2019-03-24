f0d96ee793927342d9ed09699b0ead1dab84f152
Kevin Lewis <kvn@lws.io>
2019-03-24 15:38:20 +0000

    voting mechanism built

---
+++ components/VoteState.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm" v-if='!submittedVote'>
+      <div class="input-group">
+        <label for="vote">What is your move this turn?</label>
+        <div class="ops">
+          <div class="radio">
+            <input type="radio" value="d" id="defect" v-model="form.playerVote">
+            <label for="defect">Defect</label>
+          </div>
+          <div class="radio">
+            <input type="radio" value="c" id="cooperate" v-model="form.playerVote">
+            <label for="cooperate">Cooperate</label>
+          </div>
+        </div>
+      </div>
+      <div class="input-group">
+        <label for="vote-why">Why did you make this choice?</label>
+        <textarea v-model="form.playerVoteReason" id="vote-why" required></textarea>
+      </div>
+      <div class="input-group">
+        <label for="op-vote">What do you think your opponent will do this turn?</label>
+        <div class="ops">
+          <div class="radio">
+            <input type="radio" value="-1" id="op-defect" v-model="form.expectedOpponentVote">
+            <label for="op-defect">Defect</label>
+          </div>
+          <div class="radio">
+            <input type="radio" value="1" id="op-cooperate" v-model="form.expectedOpponentVote">
+            <label for="op-cooperate">Cooperate</label>
+          </div>
+        </div>
+      </div>
+      <div class="input-group">
+
+        <label for="op-vote-why">Why do you think this?</label>
+        <textarea v-model="form.expectedOpponentVoteReason" id="op-vote-why" required></textarea>
+      </div>
+      <input type="submit" value="Submit vote">
+    </form>
+    <p v-else>Thanks for submitting your vote. Hang tight for the reveal.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.submittedVote = false;  
+  },
+  data() {
+    return {
+      submittedVote: false,
+      form: {}
+    }
+  },
+  methods: {
+    submitForm() {
+      const { gameId, userId } = this.$store.state;
+      const { name, score, side } = this.user
+
+      this.fire.votes.add({
+        gameId,
+        userId,
+        userName: name,
+        currentScore: score,
+        side,
+        round: this.game.state.round,
+        vote: { ...this.form },
+        outcome: {}
+      }).then(() => {
+        this.submittedVote = true;
+        this.vote = {};
+      })
+    }
+  },
+  props: ['user', 'fire', 'game']
+}
+</script>
--- pages/_game.vue
+++ pages/_game.vue
@@ -1,24 +1,31 @@
 <template>
-  <div>
-    <h1>Game Single</h1>
-    {{game}}
+  <div v-if='game.state'>
+    <h1>{{game.state.phase}}</h1>
+    <VoteState :fire='$firestore' :user='user' :game='game' v-if='game.state.phase == "vote"' />
   </div>
 </template>
 
 <script>
 import { db } from '~/plugins/firebase.js'
+import VoteState from '~/components/VoteState'
 
 export default {
   data() {
     return {
-      game: {}
+      game: { state: {} },
+      user: {}
     }
   },
   firestore() {
     return {
-      game: db.collection('games').doc(this.$route.params.game)
+      game: db.collection('games').doc(this.$route.params.game),
+      user: db.collection('users').doc(this.$store.state.userId),
+      votes: db.collection('votes')
     }
   },
+  components: {
+    VoteState
+  }
 }
 </script>
 
