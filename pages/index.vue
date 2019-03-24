84ec6023071781afe05486bc38c8e9853cbb3d40
Kevin Lewis <kvn@lws.io>
2019-03-24 14:05:16 +0000

    enter game lobby as user

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,87 @@
 <template>
   <div>
     <h1>Home</h1>
-    <n-link to='/1'>Game 1</n-link>
+    <form @submit.prevent='submitForm'>
+      <div class="input-group">
+        <label for="game_id">Game name</label>
+        <select id="game_id" v-model="gameId">
+          <option v-for="op in games" :key="op['.key']" :value="op['.key']">{{op.name}}</option>
+        </select>
+      </div>
+      <div class="input-group">
+        <label for="user-id">User ID</label>
+        <input type="text" v-model="userId" id="user-id" required>
+      </div>
+      <div class="first">
+        <h3>Only if joining for the first time</h3>
+        <div class="input-group">
+          <label for="side">Initial table side</label>
+          <select id="side" v-model="side">
+            <option>left</option>
+            <option>right</option>
+          </select>
+        </div>
+        <div class="input-group">
+          <label for="score">Initial score</label>
+          <input type="number" v-model='score'>
+        </div>
+      </div>
+      <input type="submit" value='Enter game'>
+    </form>
   </div>
 </template>
 
 <script>
+import { db } from '~/plugins/firebase.js'
 
+export default {
+  data() {
+    return {
+      gameId: '',
+      userId: '',
+      side: '',
+      score: '',
+      games: [],
+      users: []
+    }
+  },
+  firestore() {
+    return {
+      games: db.collection('games'),
+      users: db.collection('users')
+    }
+  },
+  methods: {
+    async submitForm() {
+      this.userId = this.userId.toUpperCase();
+      const { gameId, userId, side, score } = this;
+      const userExistsInGame = this.userExistsInGame(gameId, userId);
+      if(userExistsInGame) {
+        this.enterLobby(gameId, userExistsInGame);
+      } else {
+        this.$firestore.users.add({
+          name: userId,
+          game: gameId,
+          score: parseInt(score),
+          side: side
+        }).then(res => {
+          this.enterLobby(gameId, res.id);
+        })
+      }
+    },
+    userExistsInGame(gameId, userId) {
+      let filteredUserList = this.users.filter(user => {
+        return user.name == userId && user.game == gameId
+      });
+      return filteredUserList.length > 0 ? filteredUserList[0]['.key'] : false
+    },
+    enterLobby(gameId, userId) {
+      this.$router.push('/' + gameId)
+      this.$store.dispatch('setGameId', gameId);
+      this.$store.dispatch('setUserId', userId);
+    }
+  }
+}
 </script>
 
 <style scoped>
--- store/index.js
+++ store/index.js
@@ -1,10 +1,14 @@
 export const state = () => ({
   gameId: localStorage.getItem('gameId') || undefined,
+  userId: localStorage.getItem('userId') || undefined,
 });
 
 export const mutations = {
   setGameId(vuexContext, id) {
     vuexContext.gameId = id;
+  },
+  setUserId(vuexContext, id) {
+    vuexContext.userId = id;
   },
 }
 
@@ -12,6 +16,10 @@ export const actions = {
   setGameId(vuexContext, id) {
     vuexContext.commit('setGameId', id);
     localStorage.setItem('gameId', id);
+  },
+  setUserId(vuexContext, id) {
+    vuexContext.commit('setUserId', id);
+    localStorage.setItem('userId', id);
   },
 }
 
